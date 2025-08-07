8b3b091df498b96a821ac619fc69b80c9a7eaaad
LemmeJack <ninjapaizuri@gmail.com>
2025-08-06 23:10:35 -0400

    Update and rename 2025-03-06.js to 2025-08-06.js

--- src/js/data/2025-08-06.js
+++ src/js/data/2025-08-06.js
@@ -43,7 +43,7 @@ dataSet[dataSetVersion].options = [
 
 dataSet[dataSetVersion].characterData = [
   {
-    name: "Amatsuka Erena",
+    name: "Amatsuka Erena-天使 英玲奈",
     img: "Mahou-Amatsuka Erena.png",
     opts: {
       game: ["mahou"],
@@ -83,7 +83,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Naomin Ibah Lucile",
+    name: "Naomin Ibah Lucile-ナオミン=イヴァ=ルシル",
     img: "Mahou-Naomin Ibah Lucile.png",
     opts: {
       game: ["mahou"],
@@ -91,7 +91,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Olga",
+    name: "Olga-オルガ",
     img: "Mahou-Olga.png",
     opts: {
       game: ["mahou"],
