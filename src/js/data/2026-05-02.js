0d11c03fdf3b2d57a33c477012e979712da3bbd0
LemmeJack <ninjapaizuri@gmail.com>
2026-05-02 19:37:13 -0400

    Add new entry for 'Oppai Isekai Oppai Idol Gakuen!'

--- src/js/data/2026-05-02.js
+++ src/js/data/2026-05-02.js
@@ -14,6 +14,7 @@ dataSet[dataSetVersion].options = [
       { name: "Oppai Isekai Oppai Maid Gakuen!", key: "maid" },
       { name: "Oppai Isekai Oppai Bunny Gakuen!", key: "bunny" },
       { name: "Oppai Isekai Oppai Spy Gakuen!", key: "spy" },
+      { name: "Oppai Isekai Oppai Idol Gakuen!", key: "idol" },
     ]
   },
   {
